3d765de8d9dd957b6af15ff8f8c7d18b69d7681e
Shubham <agarwal.suvam@gmail.com>
2021-10-08 21:11:55 +0530

    updated storybook

--- src/stories/Span.stories.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Span } from "../shared/components/Span";
-
-export default {
-  title: "Components/Span",
-};
-
-export const SpanDefault = () => <Span />;
--- src/stories/Timeline.stories.jsx
+++ src/stories/Timeline.stories.jsx
@@ -1,56 +1,28 @@
 import React from "react";
-import { TextButton } from "../shared/components/Button";
-import { Card } from "../shared/components/Card";
+import { CircleIcon } from "../shared/components/Icon";
+import { Span } from "../shared/components/Span";
 import { Timeline } from "../shared/components/Timeline";
 
 export default {
   title: "Components/Timeline",
 };
 
-const events1 = [
-  {
-    status: "Ordered",
-    date: "15/10/2020 10:30",
-  },
-  {
-    status: "Processing",
-    date: "15/10/2020 14:00",
-  },
-  {
-    status: "Shipped",
-    date: "15/10/2020 16:15",
-  },
-  {
-    status: "Delivered",
-    date: "16/10/2020 10:00",
-  },
-];
+const events = ["Ordered", "Picked", "On Route", "Delivered"];
 
-const customizedContent = (item) => {
+const customizedMarker = () => {
   return (
-    <Card title={item.status} subTitle={item.date}>
-      {item.image && (
-        <img
-          src={`https://primefaces.org/primereact/showcase/showcase/demo/images/product/game-controller.jpg`}
-          onError={(e) =>
-            (e.target.src =
-              "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
-          }
-          alt={item.name}
-          width={200}
-          className="p-shadow-2"
-        />
-      )}
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
-        consequuntur error repudiandae numquam deserunt quisquam repellat libero
-        asperiores earum nam nobis, culpa ratione quam perferendis esse,
-        cupiditate neque quas!
-      </p>
-      <TextButton label="Read more"></TextButton>
-    </Card>
+    <Span>
+      <CircleIcon color="var(--secondary)" size="3x" />
+    </Span>
   );
 };
+
 export const TimelineDefault = () => (
-  <Timeline value={events1} align="alternate" content={customizedContent} />
+  <Timeline
+    layout="horizontal"
+    value={events}
+    align="top"
+    marker={customizedMarker}
+    content={(item) => item}
+  />
 );
